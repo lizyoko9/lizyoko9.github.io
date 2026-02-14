370511db0a96ec7d69b39d8e4c78dc3f37e2a22d
lizyoko9 <lizyoko9@bupt.edu.cn>
2026-02-15 04:59:22 +0800

    chore-configure-deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://lizyoko9.github.io',
   integrations: [tailwind()]
 });
